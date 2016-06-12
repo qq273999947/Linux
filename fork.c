d4d91838e660a6718b17bfa74895973fdb6348b6
root <15229234589@163.com>
2016-06-12 13:18:36 +0800

    fork process

---
+++ fork.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<errno.h>
+#include<stdlib.h>
+
+int main()
+{
+    pid_t pid = getpid();
+
+    pid_t id = fork();
+    if(id == -1){
+        printf("fork error! return code is:%d\n",errno);
+        return 2;
+    }
+    else if(id == 0){
+        printf("child pid is: %d\n",getpid());
+    }
+    else{//father
+    
+         printf("father pid id: %d\n,return pid is :%d\n",getpid(),id);
+    }
+    return 0;
+}
