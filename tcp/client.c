11469aea449e4e674c0ba514039d52a7facab9b5
root <15229234589@163.com>
2016-07-26 17:58:51 +0800

    complete socket

--- tcp/client.c
+++ tcp/client.c
@@ -7,48 +7,56 @@
 #include<netinet/in.h>
 #include<arpa/inet.h>
 
-#define SERVER_PORT 8080
-#define SERVER_IP "127.0.0.1"
-
+static void usage(const char *proc)
+{
+    printf("please use : %s [ip] [port]\n",proc);
+}
 int main(int argc,char *argv[])
 {
-    if( argc != 2 ){
-        printf("please use : client IP\n");
+    if( argc != 3 ){
+        usage(argv[0]);
+        exit(1);
+    }
+    
+    int sock = socket(AF_INET,SOCK_STREAM,0);
+    if(sock < 0){
+        perror("socket error");
         return 1;
     }
-    char *str = argv[1];
-    char buf[1024];
-    memset(buf,'\0',sizeof(buf));
-    
-    struct sockaddr_in server_sock;
-    int sock = socket(AF_INET,SOCK_STREAM,0);
-    bzero(&server_sock,sizeof(server_sock));
-    server_sock.sin_family = AF_INET;
 
-    inet_pton(AF_INET,SERVER_IP,&server_sock.sin_addr);
-    server_sock.sin_port = htons(SERVER_PORT);
+    struct sockaddr_in remote;
+    remote.sin_family = AF_INET;
+    remote.sin_port = htons(atoi(argv[2]));
+    remote.sin_addr.s_addr = inet_addr(argv[1]);
 
-    int ret = connect(sock,(struct sockaddr*)&server_sock,sizeof(server_sock));
+
+    int ret = connect(sock,(struct sockaddr*)&remote,sizeof(remote));
     if(ret < 0){
         printf("connect failed:%s\n",strerror(errno));
-        return 1;
+        return 2;
     }
     
     printf("connect success!\n");
 
+    char buf[1024];
     while(1){
-        printf("client say:");
-        fgets(buf,sizeof(buf),stdin);
-        buf[strlen(buf)-1] = '\0';
-
-        write(sock,buf,sizeof(buf));
-        if(strncasecmp(buf,"quit",4) == 0){
-            printf("qiut\n");
-            break;
+        memset(buf,'\0',sizeof(buf));
+        printf("please enter:");
+        fflush(stdout);
+        ssize_t _s = read(0,buf,sizeof(buf)-1);
+        if(_s > 0){
+            buf[_s - 1] = '\0';
+            write(sock,buf,strlen(buf));
+            _s = read(sock,buf,sizeof(buf)-1);
+            if(_s > 0){
+                if(strncasecmp(buf,"quit",4) == 0){
+                     printf("qiut\n");
+                     break;
+                 }
+                buf[_s -1] = '\0';
+                printf("%s\n",buf);
+            }
         }
-        printf("please wait...\n");
-        read(sock,buf,sizeof(buf));
-        printf("server say : %s\n",buf);
     }
     close(sock);
     return 0;
--- tcp/server.c
+++ tcp/server.c
@@ -4,27 +4,66 @@
 #include<string.h>
 #include<unistd.h>
 #include <arpa/inet.h>
+#include<pthread.h>
 
-int main()
+static void usage(const char *proc)
 {
+    printf("Please use :%s [IP] [port]\n",proc);
+}
+
+void thread_run(void *arg)
+{
+    printf("creat a new thread\n");
+    int fd = (int)arg;
+    char buf[1024];
+
+    while(1){
+        memset(buf,'\0',sizeof(buf));
+        ssize_t _s = read(fd,buf,sizeof(buf) - 1);
+        if(_s > 0){
+            buf[_s] = '\0';
+            printf("client say : %s\n",buf);
+        }    
+        memset(buf,'\0',sizeof(buf));
+        printf("please Enter: ");
+        fflush(stdout);
+        ssize_t _s2 = read(0,buf,sizeof(buf) - 1);
+        if(_s2 > 0){
+            write(fd,buf,strlen(buf));
+        }
+    }
+}
+
+int main(int argc,char *argv[])
+{
+    if(argc != 3){
+        usage(argv[0]);
+        exit(1);
+    }
+
+    //1.creat socket
+
     int sock = socket(AF_INET,SOCK_STREAM,0);
     if(sock < 0){
-        printf("creat socket error\n");
+        perror("creat socket error\n");
         return 1;
     }
 
-    struct sockaddr_in sever_socket,client_socket;
-    bzero(&sever_socket,sizeof(sever_socket));
-    sever_socket.sin_family = AF_INET;
-    sever_socket.sin_addr.s_addr = htonl(INADDR_ANY);
-    sever_socket.sin_port = htons(8080);
+    struct sockaddr_in local;
+    local.sin_family = AF_INET;
+    local.sin_port = htons(atoi(argv[2]));
+    local.sin_addr.s_addr = inet_addr(argv[1]);
 
-    if(bind(sock,(struct sockaddr*)&sever_socket,sizeof(struct sockaddr_in)) < 0){
+    //2.bind
+
+    if(bind(sock,(struct sockaddr*)&local,sizeof(local)) < 0){
         perror("bind error\n");
         close(sock);
         return 2;
     }
-    
+   
+    //3.listen
+
     if(listen(sock,10) < 0){
         perror("listen error\n");
         close(sock);
@@ -32,36 +71,28 @@ int main()
     }
 
     printf("bind and listen success!wait accept...\n");
+    
+    //4.accept
 
+    struct sockaddr_in peer;
+    socklen_t len = sizeof(peer);
     while(1){
-        socklen_t len =0;
-        int client_sock = accept(sock,(struct sockaddr*)&client_sock,&len);
-        if(client_sock < 0){
+
+        int fd = accept(sock,(struct sockaddr*)&peer ,&len);
+
+        if(fd < 0){
             perror("accept error\n");
             close(sock);
             return 4;
         }
         
-        char buf_ip[INET_ADDRSTRLEN];
-        memset(buf_ip,'\0',sizeof(buf_ip));
-        inet_ntop(AF_INET,&client_socket.sin_addr,buf_ip,sizeof(buf_ip));
+        printf("get connect,ip is : %s port is : %d\n",inet_ntoa(peer.sin_addr),ntohs(peer.sin_port));
 
-        printf("get connect,ip is : %s port is : %d\n",buf_ip,ntohs(client_socket.sin_port));
+        pthread_t id;
+        pthread_create(&id,NULL,thread_run,(void*)fd);
 
-        while(1){
-            char buf[1024];
-            memset(buf,'\0',sizeof(buf));
+        pthread_detach(id);
 
-            read(client_sock,buf,sizeof(buf));
-            printf("client say : %s\n",buf);
-
-            printf("sever say :\n");
-            memset(buf,'\0',sizeof(buf));
-            fgets(buf,sizeof(buf),stdin);
-            buf[strlen(buf) - 1] = '\0';
-            write(client_sock, buf, strlen(buf)+1);
-            printf("please wait...\n");
-        }
     }
     close(sock);
     return 0;
